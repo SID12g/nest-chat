0ceeda7d45ed8ffbae8fb59f2d578d626e1ba4f5
SID12g <sid12g@naver.com>
2024-02-23 21:12:11 +0900

    add socket

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { ConfigModule } from '@nestjs/config';
+import mongoose from 'mongoose';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ChatsModule } from './chats/chats.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRoot(process.env.MONGODB_URI),
+    ChatsModule,
+  ],
   controllers: [AppController],
   providers: [],
 })
-export class AppModule {}
+export class AppModule {
+  configure() {
+    const DEBUG = process.env.MODE === 'dev' ? true : false;
+    mongoose.set('debug', DEBUG);
+  }
+}
---
+++ src/chats/chats.module.ts
@@ -0,0 +1,5 @@
+import { Module } from '@nestjs/common';
+import { ChatsGateway } from './chats.gateway';
+
+@Module({ providers: [ChatsGateway] })
+export class ChatsModule {}
